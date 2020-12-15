b84124e8b1405d410a6d9eff801a91b289f16c84
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-12-15 16:08:57 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   resources :dogs
-  resources :employees
+  
+  # resources :employees
+  get '/employees', to: "employees#index", as: "employees"
+  get '/employees/:id', to: "employees#show", as: "employee"
+  get '/employees/new', to: "employees#new", as: "new_employee"
+  post '/employees', to: "employees#create"
+
+
+  get '/employees/:id/edit/', to: "employees#edit", as: "edit_employee"
+  patch '/employees/:id', to: "employees#update"
+  get '/employees/:id', to: "employees#show", as: "update_employee" 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
